dc9bfe627418be7c7752db56cdad523dc0c6c665
Nikolai Ochirov <dubdubman@ya.ru>
2022-02-13 15:31:34 +0300

    correction in game-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-import playIsEven from '../src/games/even.js';
+import playEven from '../src/games/even.js';
 import { greetings, getPlayerName, sayHello } from '../src/cli.js';
 
 greetings();
 const playerName = getPlayerName();
 sayHello(playerName);
-playIsEven(playerName);
+playEven(playerName);
