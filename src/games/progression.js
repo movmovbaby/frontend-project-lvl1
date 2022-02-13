8dc1554eaa8460b85b1d66b2e289164d28d207f0
Nikolai Ochirov <dubdubman@ya.ru>
2022-02-13 22:29:16 +0300

    fix error in brain-progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,12 +3,13 @@ import { gameLoop, displayString } from '../index.js';
 const task = 'What number is missing in the progression?';
 const maxStartNumber = 10;
 const maxStep = 6;
+const range = 10;
+const startMissIndex = 2;
 
 const generateProgression = () => {
   const start = Math.floor(Math.random() * maxStartNumber);
   const step = Math.floor(Math.random() * maxStep);
-  const range = 10;
-  const missingIndex = Math.floor(Math.random() * range);
+  const missingIndex = Math.floor(Math.random() * range - startMissIndex + 1) + startMissIndex;
   const progression = [];
 
   for (let i = 0; i < range; i += 1) {
@@ -17,13 +18,14 @@ const generateProgression = () => {
 
   progression[missingIndex] = '..';
 
-  return progression;
+  return progression.join(' ');
 };
 
 const findMissingItem = (progression) => {
-  const missingIndex = progression.indexOf('..');
-  const start = progression[0];
-  const step = progression[1] - progression[0];
+  const progressionArray = progression.split(' ');
+  const missingIndex = Number(progressionArray.indexOf('..'));
+  const start = Number(progressionArray[0]);
+  const step = Number(progressionArray[1]) - Number(progressionArray[0]);
   const answer = start + step * missingIndex;
   return answer;
 };
