1689eb9fea65b0f27f6d952214d437c1d0f1f36e
Nikolai Ochirov <dubdubman@ya.ru>
2022-02-16 23:19:53 +0300

    remove unnecessery constant

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,3 @@
-const maxNumber = 100;
-const generateRandomInt = (min = 1, max = 100) => {
-  return Math.floor(Math.random() * (max - min)) + min;
-};
+const generateRandomInt = (min = 1, max = 100) => Math.floor(Math.random() * (max - min)) + min;
 
 export default generateRandomInt;
