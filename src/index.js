6bb95e021c70ab068855143df3622e002ad434ec
Kolya Ochorov <dubdubman@ya.ru>
2022-02-22 19:24:19 +0300

    change game engine to work with questions and answers list

--- src/index.js
+++ src/index.js
@@ -3,18 +3,20 @@ import readlineSync from 'readline-sync';
 const correctMessage = 'Correct!';
 const numberOfGames = 3;
 
-const gameLoop = (problemGenerator, problemSolver, playerName, rounds = numberOfGames) => {
+const gameLoop = (gameData, task) => {
+  console.log('Welcome to the Brain Games!');
+  const playerName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${playerName}!`);
+  console.log(task);
+
   let currentRound = 0;
-  let answer = '';
-  let userAnswer = '';
   let returnString = '';
 
-  while (currentRound !== rounds) {
-    const problem = problemGenerator();
-    const question = `Question: ${problem}`;
-    console.log(question);
-    answer = problemSolver(problem);
-    userAnswer = readlineSync.question('Your answer: ');
+  while (currentRound !== numberOfGames) {
+    const question = gameData[currentRound][0];
+    console.log(`Question: ${question}`);
+    const answer = gameData[currentRound][1];
+    const userAnswer = readlineSync.question('Your answer: ');
 
     if (userAnswer === answer) {
       console.log(correctMessage);
